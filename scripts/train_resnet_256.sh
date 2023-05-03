936df4f3c5ef27e8d8745b26fc5ae8e2773070ce
Zhiyong Guo <guozy0103@outlook.com>
2023-05-03 14:39:04 -0400

    feat: train 3 epochs and 3 batches

--- scripts/train_resnet_256.sh
+++ scripts/train_resnet_256.sh
@@ -3,7 +3,7 @@ TRAIN_SET=./Dataset/kitti_256/
 python train.py $TRAIN_SET \
 --dispnet DispResNet \
 --num-scales 1 \
--b4 -s0.1 -c0.5 --epoch-size 1000 --sequence-length 3 \
+-b4 -s0.1 -c0.5 --epoch-size 3 --sequence-length 3 \
 --with-mask \
 --with-ssim \
 --with-gt \
