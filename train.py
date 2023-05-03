890380ce2ae41d26b82dd1b629c1b4616ef928b0
Zhiyong Guo <guozy0103@outlook.com>
2023-05-02 20:41:13 -0400

    feat: print epoch

--- train.py
+++ train.py
@@ -319,7 +319,7 @@ def train(args, train_loader, disp_net, pose_net, optimizer, epoch_size, logger,
         logger.train_bar.update(i+1)
         if i % args.print_freq == 0:
             logger.train_writer.write(
-                'Train: Time {} Data {} Loss {}'.format(batch_time, data_time, losses))
+                'Train: Epoch {} Time {}s Data {} Loss {}'.format(i, batch_time, data_time, losses))
         if i >= epoch_size - 1:
             break
 
