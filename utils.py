a46fd707c053f383ee5e85651b36782e483883fa
Zhiyong Guo <guozy0103@outlook.com>
2023-05-04 20:59:23 -0400

    feat: print is_best

--- utils.py
+++ utils.py
@@ -70,9 +70,10 @@ def save_checkpoint(save_path, dispnet_state, exp_pose_state, is_best, filename=
     for (prefix, state) in zip(file_prefixes, states):
         print('saving model to:', save_path/'{}_{}'.format(prefix, filename))
         torch.save(state, save_path/'{}_{}'.format(prefix, filename))
-        torch.save(state, os.path.join('checkpoints', prefix, filename))
+        torch.save(state, os.path.join('checkpoints', prefix, filename))        
 
     if is_best:
+        print(f"==>> is_best: {is_best}")
         for prefix in file_prefixes:
             shutil.copyfile(save_path/'{}_{}'.format(prefix, filename),
                             save_path/'{}_model_best.pth.tar'.format(prefix))
