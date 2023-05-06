28cf876f744b9f9e6c410b5308999c4f3bc6f7f2
Zhiyong Guo <guozy0103@outlook.com>
2023-05-06 18:19:50 -0400

    feat: depth inference

---
+++ scripts/run_inference.sh
@@ -0,0 +1,6 @@
+OUTPUT_DIR=results/
+DISP_NET=checkpoints/dispnet_model_best.pth.tar
+DATA_ROOT=Dataset/kitti_depth_test
+
+python3 run_inference.py --pretrained $DISP_NET \
+    --dataset-dir $DATA_ROOT/color --output-dir $OUTPUT_DIR --output-disp
