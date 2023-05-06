87d43247bd70a5455346d3280f46fd6fb022e634
Zhiyong Guo <guozy0103@outlook.com>
2023-05-06 18:45:47 -0400

    feat: pretained net

--- scripts/run_inference.sh
+++ scripts/run_inference.sh
@@ -2,5 +2,5 @@ OUTPUT_DIR=results/
 DISP_NET=checkpoints/dispnet_model_best.pth.tar
 DATA_ROOT=Dataset/kitti_depth_test
 
-python3 run_inference.py --pretrained $DISP_NET \
+python3 run_inference.py --pretrained-dispnet $DISP_NET \
     --dataset-dir $DATA_ROOT/color --output-dir $OUTPUT_DIR --output-disp --dispnet DispResNet
